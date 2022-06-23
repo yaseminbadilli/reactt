98d3899a7222b572a07d66c5e4befdb6fa91877c
yaseminbadilli <yaseminbadilli@hotmail.com>
2022-06-23 20:27:28 +0300

    ders02

--- src/Categories.js
+++ src/Categories.js
@@ -1,34 +1,35 @@
 import React from "react"
-import {ListGroup, ListGroupItem} from "reactstrap";
-export default class Categories extends React.Component{
+import { ListGroup, ListGroupItem } from "reactstrap";
+export default class Categories extends React.Component {
 
 
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
-render()
-{
-    return( 
-    <div>
-        <h2>{this.props.info.title}</h2>
-        <ListGroup>
 
-            
-            <ListGroupItem>Kovalar</ListGroupItem>
-            <ListGroupItem>Tek Tavuk Ürünleri</ListGroupItem>
-            <ListGroupItem>Sandviç ve Salata</ListGroupItem>
-            <ListGroupItem>Çocuklar</ListGroupItem>
-            <ListGroupItem>Tatlılar</ListGroupItem>
-            <ListGroupItem>Ek lezzetletler ve Ürünler</ListGroupItem>
-            <ListGroupItem>İçecekler</ListGroupItem>
-            <ListGroupItem>Soslar</ListGroupItem>
+    render() {
 
-        </ListGroup>
-    </div>)
-    
-   
-}
+        return (
+            <div>
+                <h2>{this.props.info.title}</h2>
+                <ListGroup>
+
+
+                    <ListGroupItem>Kovalar</ListGroupItem>
+                    <ListGroupItem>Tek Tavuk Ürünleri</ListGroupItem>
+                    <ListGroupItem>Sandviç ve Salata</ListGroupItem>
+                    <ListGroupItem>Çocuklar</ListGroupItem>
+                    <ListGroupItem>Tatlılar</ListGroupItem>
+                    <ListGroupItem>Ek lezzetletler ve Ürünler</ListGroupItem>
+                    <ListGroupItem>İçecekler</ListGroupItem>
+                    <ListGroupItem>Soslar</ListGroupItem>
+
+                </ListGroup>
+            </div>)
+
+
+    }
 
 
 }
--- src/Products.js
+++ src/Products.js
@@ -1,66 +1,67 @@
 import React from "react";
-import { ListGroup, ListGroupItem ,Card,CardImg} from "reactstrap";
+import { ListGroup, ListGroupItem, Card, CardImg } from "reactstrap";
 
 
 
-export default class Product extends React.Component{
-    constructor(props){
+export default class Product extends React.Component {
+    constructor(props) {
         super(props);
     }
 
-    render(){
+    render() {
 
-        return( <div>
+        return (<div>
             <h2>{this.props.info.title}</h2>
 
             <ListGroup>
 
-            
-            <ListGroupItem>
-                Kovalar
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133"/></Card>
+
+                <ListGroupItem>
+                    Kovalar
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133" /></Card>
 
                 </ListGroupItem>
 
 
-            <ListGroupItem>
-                Tek Tavuk Ürünleri
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/10lu-dipn-chicken.png?v=133"/></Card>
-            </ListGroupItem>
-
-            <ListGroupItem>
-                Sandviç ve Salata
-                <Card><CardImg src="https://i4.hurimg.com/i/hurriyet/75/750x422/5b6afeb85d1c2e1f9ce41225.jpg"/></Card>
-
-            </ListGroupItem>
-
-            <ListGroupItem>
-                Çocuklar
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133"/></Card>
-
+                <ListGroupItem>
+                    Tek Tavuk Ürünleri
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/10lu-dipn-chicken.png?v=133" /></Card>
                 </ListGroupItem>
-            <ListGroupItem>
-                Tatlılar
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133"/></Card>
 
-                </ListGroupItem>
-            <ListGroupItem>
-                Ek lezzetletler ve Ürünler
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133"/></Card>
-
-                </ListGroupItem>
-            <ListGroupItem>
-                İçecekler
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133"/></Card>
-
-            </ListGroupItem>
-            <ListGroupItem>
-                Soslar
-                <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133"/></Card>
+                <ListGroupItem>
+                    Sandviç ve Salata
+                    <Card><CardImg src="https://i4.hurimg.com/i/hurriyet/75/750x422/5b6afeb85d1c2e1f9ce41225.jpg" /></Card>
 
                 </ListGroupItem>
 
-        </ListGroup>
+                <ListGroupItem>
+                    Çocuklar
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133" /></Card>
+
+                </ListGroupItem>
+                <ListGroupItem>
+                    Tatlılar
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133" /></Card>
+
+                </ListGroupItem>
+                <ListGroupItem>
+                    Ek lezzetletler ve Ürünler
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133" /></Card>
+
+                </ListGroupItem>
+                <ListGroupItem>
+                    İçecekler
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133" /></Card>
+
+                </ListGroupItem>
+                <ListGroupItem>
+                    Soslar
+                    <Card><CardImg src="https://www.popeyes.com.tr/cmsfiles/products/2-kisilik-kemiksiz-kova-menu.png?v=133" /></Card>
+
+                </ListGroupItem>
+
+            </ListGroup>
         </div>)
-       
-}}
+
+    }
+}
